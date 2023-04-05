1898de0d7f1aeb71c7d3e7a062185ba7d71167a1
PJ <pierre-joseph.beaugendre@epitech.eu>
2023-04-05 12:19:22 +0200

    mobile search bar upgraded

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -41,7 +41,7 @@ const SearchBar = () => {
         <div className={styles.text_input_container}>
           <input type="text"  placeholder='Mojito...' className={styles.text_input} onChange={(e) => {updateQuery(e)}}/>
           <Link href={{ pathname: '/', query: { query } }}>
-            <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Search_Magnifying_Glass"> <path id="Vector" d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g> </g></svg>
+            <svg width="30px" height="30px" className={styles.search_svg} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / Search_Magnifying_Glass"> <path id="Vector" d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g> </g></svg>
             <input type="submit" id="myBtn" value="Submit" className={styles.enter_key}/>
           </Link>
         </div>
